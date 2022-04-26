9cb8d5484ef1694d1fbe42ce4c0c6b68e1fbc5bc
Charles Dawson <charles.dwsn@gmail.com>
2022-04-26 13:15:55 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requirements = [
+    "casadi",
+    "matplotlib",
+    "numpy",
+    "seaborn",
+    "tqdm",
+]
+
+dev_requirements = [
+    "black",
+    "mypy",
+    "pytest",
+    "flake8",
+]
+
+setup(
+    name="mpc",
+    version="0.0.0",
+    description="Simple nonlinear MPC for obstacle avoidance",
+    author="Charles Dawson",
+    author_email="cbd@mit.edu",
+    url="https://github.com/dawsonc/obstacle_avoidance_mpc",
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements},
+    packages=find_packages(),
+)
